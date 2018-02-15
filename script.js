7f6a3cfbe563b238e6311af091573b3c3329571e
Arnold Sztandarski <sztandarski.arnold@gmail.com>
2018-02-15 18:30:13 +0100

    Add styles

--- script.js
+++ script.js
@@ -5,12 +5,7 @@ class Stopwatch extends React.Component {
             running: false,
             fomrat: ``,
             lp: 0,
-            results: [
-                {
-                    lp: '',
-                    score: ''
-                },
-            ],
+            results: [],
             time: {
                 minutes: 0,
                 seconds: 0,
@@ -22,6 +17,8 @@ class Stopwatch extends React.Component {
         if (!this.state.running) {
             this.setState({running: true});
             this.watch = setInterval(() => this.count(), 10);
+            this.setState({lp: ++this.state.lp});
+            console.log(this.state.results);
         }
     }
     count() {
@@ -35,18 +32,31 @@ class Stopwatch extends React.Component {
             this.setState({seconds: this.state.time.seconds = 0});
         }
     }
-        
     stop() {
         this.setState({running: false});
         clearInterval(this.watch);
     }
     save() {
-            if (this.state.running) {
-                this.setState({lp: ++this.state.lp});
-                this.state.results.push(this.state.lp, this.state.format);
-                console.log(this.state.results);
-                console.log(this.state.counter);
-            }
+        let formatStr = this.state.format;
+        formatStr.toString();
+        let args = this.state.results;
+        let check = args.length; 
+        let obj = {
+            lp: this.state.lp,
+            format: formatStr
+        }
+
+        if (this.state.running) {
+            this.setState({lp: ++this.state.lp});
+            this.state.results.push(obj);
+            check = args.length;
+    
+        } else if (this.state.lp !== check) {
+            this.setState({lp: this.state.lp});
+            this.state.results.push(obj);
+            check = args.length;
+
+        }
     }
     reset() {
         this.setState({miliseconds: this.state.time.miliseconds = 0});
@@ -54,28 +64,43 @@ class Stopwatch extends React.Component {
         this.setState({minutes: this.state.time.minutes = 0});
     }
     deleteAll() {
-
+        this.setState({results: []});
+        this.setState({lp: 0});
+        this.reset();
     }
 
     render() {
         let counter = 0;
-        this.state.format = `${this.state.time.minutes}:${this.state.time.seconds}:${this.state.time.miliseconds}`;
+        function pad0(value) {
+            let pad = value.toString();
+            if (pad.length < 2) {
+                pad = '0' + pad;
+            }
+            return pad;
+        };
+        this.state.format = `${pad0(this.state.time.minutes)} : ${pad0(this.state.time.seconds)} : ${pad0(this.state.time.miliseconds)}`;
         let list = this.state.results.map((result, index) => 
-            <li key={index}>Czas nr. {result.lp} : {result.score}</li>
+            <li key={index}>Time No. {result.lp} = {result.format}</li>
         );
+
         return(
             <div>
-                <nav>
+                <div className="title"><h1>Stopwatch in React.js</h1></div>
+                <div className="main">
+                    <div className="time"><span>{pad0(this.state.time.minutes)}</span></div>
+                    <div className="colon"><p>:</p></div>
+                    <div className="time"><span>{pad0(this.state.time.seconds)}</span></div>
+                    <div className="colon"><p>:</p></div>
+                    <div className="time"><span>{pad0(this.state.time.miliseconds)}</span></div>
+                </div>
+                <div className="nav">
                     <button onClick={this.start.bind(this)}>Start</button>
                     <button onClick={this.stop.bind(this)}>Stop</button>
                     <button onClick={this.save.bind(this)}>Save</button>
                     <button onClick={this.reset.bind(this)}>Reset</button>
-                    <button>Delete all</button>
-                </nav>
-                <main>
-                    <span>{this.state.format}</span>
-                </main>
-                <div>
+                    <button onClick={this.deleteAll.bind(this)}>Delete all</button>
+                </div>
+                <div className="list">
                     <ul>
                         {list}
                     </ul>
